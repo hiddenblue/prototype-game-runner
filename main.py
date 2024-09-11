c53b7cea2b42c98f081b219b746bb133debfc03f
hiddenblue <62304226+hiddenblue@users.noreply.github.com>
2024-09-12 00:12:38 +0800

    adjust the cpu limit parameter, and add some code comments

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from tkinter import filedialog
 
 def set_cpu_affinity(exe_path, cpu_limit):
     process = psutil.Popen(exe_path)
+    # os.system执行类似于直接通过系统的shell执行程序，而Pipeopen类似subprocess， 这样更方便重定向程序的标准输入输出，方便操纵
     pid = process.pid
-    affinity = list(range(cpu_limit))
+    affinity = list(range(cpu_limit))  #  a list start from 0, including integers.
     psutil.Process(pid).cpu_affinity(affinity)
 
 
@@ -18,20 +19,20 @@ def browse_button_func():
 
 def run_game():
     exe_path = entry_path.get()
-    cpu_limit = 2
+    cpu_limit = 4  # win11下 prototype 超过10个逻辑核心就会出现闪退，在为2时略有卡顿，在设置到4后帧率明显有提升，但是继续增加效果不明显，还有少量副作用
     if exe_path:
         set_cpu_affinity(exe_path, cpu_limit)
 
 
 def create_main_window():
     window = tk.Tk()
-    window.resizable(False, False)
+    window.resizable(True, True)
     window.title("CPU Limiter")
 
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    window_width = 250
-    window_height = 135
+    window_width = 300
+    window_height = 200
     x = (screen_width - window_width) // 2
     y = (screen_height - window_height) // 2
     window.geometry(f"{window_width}x{window_height}+{x}+{y}")
@@ -41,9 +42,9 @@ def create_main_window():
 
 def create_widgets(window):
     label = tk.Label(window, text="Select the prototype game's .exe file:")
-    label.pack(pady=5, padx=10)
+    label.pack(pady=10, padx=10)
 
-    entry_path = tk.Entry(window)
+    entry_path = tk.Entry(window)  # 这里是创建一个单行的文本框
     entry_path.pack(pady=5)
 
     browse_button = tk.Button(window, text="Browse", command=browse_button_func)
